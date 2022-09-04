903dee68a1698260be0217c6245f58216d51053d
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-04 14:33:38 +0800

    feat: project init

--- app.js
+++ app.js
@@ -37,16 +37,7 @@ app.get('/search', (req, res) => {
   if (condition === 'name') {
     const stores = storeList.results.filter(store => {
       return store.name.includes(keyword)
-    })
-
-    // not found
-    if (stores.length === 0) {
-      conditionResult = '沒有您要找的資料 !!!'
-      return res.render('index', { stores: storeList.results, keyword, name: condition, conditionResult })
-    }
-
-    conditionResult = `發現:${stores.length} 筆`
-    return res.render('index', { stores: stores, keyword, name: condition, conditionResult })
+    }) 
   }
 
   //filter 餐廳類別.category
